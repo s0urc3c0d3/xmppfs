f8a6c8fe2fea23ddc77bb809c60c2d082709054a
Grzegorz Dwornicki <g.r.d0110@gmail.com>
2012-07-19 19:08:10 +0200

    added receiving roster from xmpp server, TODO: create buffer for FUSE (readdir function)

--- xmppfs.c
+++ xmppfs.c
@@ -5,6 +5,7 @@
 #include <fuse/fuse.h>
 #include <fuse/fuse_opt.h>
 #include <strophe.h>
+#include <pthread.h>
 
 
 static int xmppfs_getattr(const char *filename, struct stat *fstat)
@@ -21,9 +22,69 @@ static struct fuse_operations xmppfs = {
 	//.readdir = xmppfs_readdir
 };
 
+//  XMPP_part
+
+int xmpp_connection_handle_reply(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza, void * const userdata)
+{
+	xmpp_stanza_t *query, *item;
+	char *type, *name;
+
+	type = xmpp_stanza_get_type(stanza);
+	if (strcmp(type,"error") == 0)
+		fprintf(stderr, "ERROR: query failed\n");
+	else {
+		query = xmpp_stanza_get_child_by_name(stanza, "query");
+		//zaczynamy pobieranie rostera
+		for (item = xmpp_stanza_get_children(query); item;
+			item = xmpp_stanza_get_next(item))
+				if ((name = xmpp_stanza_get_attribute(item, "name")))
+					//xmpp_stanza_get_attribute(item, "jid")
+					//name - zawiera nazwe wyswietana
+				else
+					//xmpp_stanza_get_attribute(item, "jid")
+	}
+	//wskakujemy do watku
+
+	xmpp_disconnect(conn);
+
+	return 0;
+}
+					
+
+void xmpp_connection_handler(xmpp_conn_t * const conn, const xmpp_conn_event_t status, const int error, xmpp_stream_error_t * const stream_error, void * const userdata)
+{
+	xmpp_ctx_t *ctx = (xmpp_ctx_t *)userdata;
+	xmpp_stanza_t *iq, *query;
+
+	if (status == XMPP_CONN_CONNECT) {
+		fprintf(stderr, "DEBUG: connected\n");
+
+		xmpp_stanza_set_name(iq,"iq");
+		xmpp_stanza_set_type(iq, "get");
+		xmpp_stanza_set_id(iq, "roster1");
+
+		query = xmpp_stanza_new(ctx);
+		xmpp_stanza_set_name(query, "query");
+		xmpp_stanza_set_ns(query, XMPP_NS_ROSTER);
+
+		xmpp_stanza_add_child(iq, query);
+
+		xmpp_stanza_release(query)l
+
+		xmpp_id_handler_add(conn, xmpp_connection_handle_reply, "roster1", ctx);
+
+		xmpp_send(conn, iq);
+
+		xmpp_stanza_release(iq);
+	} else {
+		fprint(stderr, "DEBUG: disconnected\n");
+		xmpp_stop(ctx);
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	char *user_jid="testet@example.jabber.com/debian";
+	char *user_jid="tester@example.jabber.com/debian";
 	char *user_pass="tester";
 	char *host="example.jabber.com";
 	int port = 5222;
