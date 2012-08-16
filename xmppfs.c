56805d2037232857573096243ab3b7202e6e6736
Grzegorz Dwornicki <g.r.d0110@gmail.com>
2012-08-16 15:35:06 +0200

    fuse getattr breaks program on line 101 - something is wrong with time_of_presence. Need more debug on this...

--- xmppfs.c
+++ xmppfs.c
@@ -11,7 +11,8 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <string.h>
-#include "/root/xmpp_libs/libstrophe-1.0.0/src/common.h"
+//#include "/root/xmpp_libs/libstrophe-1.0.0/src/common.h"
+#include "/root/libstrophe-1.0.0/src/common.h"
 
 struct _xmpp_contact_list {
 	char *jid;
@@ -39,6 +40,7 @@ static int xmppfs_getattr(const char *filename, struct stat *fstat)
 	int res = 0;
 	char /**no_root_slash,*next_slash,*/ *substr;
 	struct tm *tmp_time;
+	time_t time_of_presence;
 //system("echo dupa > /root/dupa");	
 	
 	memset(fstat,0,sizeof(struct stat));
@@ -64,7 +66,7 @@ static int xmppfs_getattr(const char *filename, struct stat *fstat)
 
 	//while(tmp->next != NULL )
 	//{
-		if (strncmp(tmp->jid,filename,strlen(filename)) && !tmp->stamp != NULL)
+		if (strncmp(tmp->jid,filename,strlen(filename)) && tmp->stamp != NULL)
 		{
 			substr=(char *)malloc(5);
 			memset(substr,0,5);
@@ -73,24 +75,31 @@ static int xmppfs_getattr(const char *filename, struct stat *fstat)
 			tmp_time->tm_year=atoi(substr);
 
 			memset(substr,0,5);
-			strncpy(substr, tmp->stamp+5, 2);
+			strncpy(substr, tmp->stamp+4, 2);
 			tmp_time->tm_mon=atoi(substr);
 
 			memset(substr,0,5);
-			strncpy(substr, tmp->stamp+8, 2);
+			strncpy(substr, tmp->stamp+6, 2);
 			tmp_time->tm_mday=atoi(substr);
 
 			memset(substr,0,5);
-			strncpy(substr, tmp->stamp+11, 2);
+			strncpy(substr, tmp->stamp+9, 2);
 			tmp_time->tm_hour=atoi(substr);
 
 			memset(substr,0,5);
-			strncpy(substr, tmp->stamp+14, 2);
+			strncpy(substr, tmp->stamp+12, 2);
 			tmp_time->tm_min=atoi(substr);
 
 			memset(substr,0,5);
-			strncpy(substr, tmp->stamp+17, 2);
+			strncpy(substr, tmp->stamp+15, 2);
 			tmp_time->tm_sec=atoi(substr);
+			
+			char *s;
+			sprintf(s,"echo %s %i %i %i %i %i %i > /root/dupa5",tmp->stamp,tmp_time->tm_year,tmp_time->tm_mon,tmp_time->tm_mday,tmp_time->tm_hour,tmp_time->tm_min,tmp_time->tm_sec);
+			system(s);
+			time_of_presence = mktime(tmp_time);
+			fstat->st_ctime = time_of_presence;
+	system("echo > /root/dupa5");
 		}
 	//}
 		
@@ -114,7 +123,7 @@ static int xmppfs_readdir(const char *dirname, void *buf, fuse_fill_dir_t filler
 	//if (tmp->next != NULL) system("echo null found! > /root/dupa");
 
 	//char *s;
-	while (tmp->next->next !=NULL)
+	while (tmp->jid != NULL && tmp->next->next != NULL)
 	{
 		//sprintf(s,"echo %s >> /dupa/root3",tmp->jid);
 		//system(s);
@@ -210,10 +219,10 @@ int presence_handler(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza, voi
 	x = xmpp_stanza_get_child_by_name(stanza, "x");
 	stamp = xmpp_stanza_get_attribute(x, "stamp");
 	from = xmpp_stanza_get_attribute(stanza, "from");
-
-	while (tmp->next != NULL)
+// added second condition to while - tmp_jid != NULL because sometimes segfault was raised
+	while (tmp->next != NULL && tmp->jid != NULL)
 	{
-		fprintf(stderr,"%s %s\n",tmp->jid,from);
+//		fprintf(stderr,"%s %s\n",tmp->jid,from);
 		
 		if (strncmp(tmp->jid,from,strlen(from)))
 		{
